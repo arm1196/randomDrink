63176171c6d13b943d2babb7ed3aed205dcace2a
adisonTip <86033390+adisonTip@users.noreply.github.com>
2025-02-04 22:04:45 +0700

    ok2

---
+++ src/data/messages.js
@@ -0,0 +1,24 @@
+// src/data/messages.js
+import img1 from '../img/ice1.jpeg';
+import img2 from '../img/coconut-juice.jpg';
+import img3 from '../img/m2.jpg';
+
+const messages = [
+  {
+    title: 'สูตร 18',
+    description: 'ผลไม้ + โยเกิร์ต (ธรรมชาติ)',
+    imageUrl: img1,
+  },
+  {
+    title: 'สูตร 08',
+    description: 'มะพร้าว + นมเปรี้ยว',
+    imageUrl: img2,
+  },
+  {
+    title: 'สูตร 34',
+    description: 'มะม่วง + นมเปรี้ยว',
+    imageUrl: img3,
+  },
+];
+
+export default messages;
