c845792b38de52b4ecf49b10f686926f0df2ea9b
adisonTip <86033390+adisonTip@users.noreply.github.com>
2025-02-05 11:49:40 +0700

    update meaase

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,24 +1,369 @@
 // src/data/messages.js
-import img1 from '../img/ice1.jpeg';
-import img2 from '../img/coconut-juice.jpg';
-import img3 from '../img/m2.jpg';
+import img1 from '../img/01.jpg';
+import img2 from '../img/02.jpg';
+import img3 from '../img/03.jpg';
+import img4 from '../img/04.jpg';
+import img5 from '../img/05.jpg';
+import img6 from '../img/06.jpg';
+import img7 from '../img/07.jpg';
+import img8 from '../img/08.jpg';
+import img9 from '../img/09.jpg';
+import img10 from '../img/10.jpg';
+import img11 from '../img/11.jpg';
+import img12 from '../img/12.jpg';
+import img13 from '../img/13.jpg';
+import img14 from '../img/14.jpg';
+import img15 from '../img/15.jpg';
+import img16 from '../img/16.jpg';
+import img17 from '../img/17.jpg';
+import img18 from '../img/18.jpg';
+import img19 from '../img/19.jpg';
+import img20 from '../img/20.jpg';
+import img21 from '../img/21.jpg';
+import img22 from '../img/22.jpg';
+import img23 from '../img/23.jpg';
+import img24 from '../img/24.jpg';
+import img25 from '../img/25.jpg';
+import img26 from '../img/26.jpg';
+import img27 from '../img/27.jpg';
+import img28 from '../img/28.jpg';
+import img29 from '../img/29.jpg';
+import img30 from '../img/30.jpg';
+import img31 from '../img/31.jpg';
+import img32 from '../img/32.jpg';
+import img33 from '../img/33.jpg';
+import img34 from '../img/34.jpg';
+import img35 from '../img/35.jpg';
+import img36 from '../img/36.jpg';
+import img37 from '../img/37.jpg';
+import img38 from '../img/38.jpg';
+import img39 from '../img/39.jpg';
+import img40 from '../img/40.jpg';
+import img41 from '../img/41.jpg';
+import img42 from '../img/42.jpg';
+import img43 from '../img/43.jpg';
+import img44 from '../img/44.jpg';
+import img45 from '../img/45.jpg';
+import img46 from '../img/46.jpg';
+import img47 from '../img/47.jpg';
+import img48 from '../img/48.jpg';
+import img49 from '../img/49.jpg';
+import img50 from '../img/50.jpg';
+import img51 from '../img/51.jpg';
+import img52 from '../img/52.jpg';
+import img53 from '../img/53.jpg';
+import img54 from '../img/54.jpg';
+import img55 from '../img/55.jpg';
+import img56 from '../img/56.jpg';
+import img57 from '../img/57.jpg';
+import img58 from '../img/58.jpg';
+import img59 from '../img/59.jpg';
+
 
 const messages = [
   {
     title: 'สูตร 01',
-    description: 'ผลไม้ + โยเกิร์ต (ธรรมชาติ)',
+    description: 'นมเปรี้ยว + ปีโป้',
     imageUrl: img1,
   },
   {
     title: 'สูตร 02',
-    description: 'มะพร้าว + นมเปรี้ยว',
+    description: 'นมเปรี้ยว + ลำไย',
     imageUrl: img2,
   },
   {
     title: 'สูตร 03',
-    description: 'มะม่วง + นมเปรี้ยว',
+    description: 'นมเปรี้ยว + เงาะ',
     imageUrl: img3,
   },
+  {
+    title: 'สูตร 04',
+    description: 'นมเปรี้ยว + นูเทลล่า',
+    imageUrl: img4,
+  },
+  {
+    title: 'สูตร 05',
+    description: 'นมเปรี้ยว + มันม่วง',
+    imageUrl: img5,
+  },
+  {
+    title: 'สูตร 06',
+    description: 'นมเปรี้ยว + ฟรุ๊ตตี้',
+    imageUrl: img6,
+  },
+  {
+    title: 'สูตร 07',
+    description: 'นมเปรี้ยว + โกโก้',
+    imageUrl: img7,
+  },
+  {
+    title: 'สูตร 08',
+    description: 'นมเปรี้ยว + สับปะรด',
+    imageUrl: img8,
+  },
+  {
+    title: 'สูตร 09',
+    description: 'นมเปรี้ยว + โอริโอ้',
+    imageUrl: img9,
+  },
+  {
+    title: 'สูตร 10',
+    description: 'นมเปรี้ยว + กล้วย',
+    imageUrl: img10,
+  },
+  {
+    title: 'สูตร 11',
+    description: 'นมเปรี้ยว + มิ้นท์',
+    imageUrl: img11,
+  },
+  {
+    title: 'สูตร 12',
+    description: 'นมเปรี้ยว + แก้วมังกร',
+    imageUrl: img12,
+  },
+  {
+    title: 'สูตร 13',
+    description: 'นมเปรี้ยว + M100',
+    imageUrl: img13,
+  },
+  {
+    title: 'สูตร 14',
+    description: 'นมเปรี้ยว + มะม่วง',
+    imageUrl: img14,
+  },
+  {
+    title: 'สูตร 15',
+    description: 'นมเปรี้ยว + น้ำผึ้ง',
+    imageUrl: img15,
+  },
+  {
+    title: 'สูตร 16',
+    description: 'นมเปรี้ยว + สตรอว์เบอร์รี่',
+    imageUrl: img16,
+  },
+  {
+    title: 'สูตร 17',
+    description: 'นมเปรี้ยว + บลูเบอร์รี่',
+    imageUrl: img17,
+  },
+  {
+    title: 'สูตร 18',
+    description: 'นมเปรี้ยว + กาแฟ',
+    imageUrl: img18,
+  },
+  {
+    title: 'สูตร 19',
+    description: 'นมเปรี้ยว + อะโวคาโด',
+    imageUrl: img19,
+  },
+  {
+    title: 'สูตร 20',
+    description: 'นมเปรี้ยว + แอปเปิ้ล',
+    imageUrl: img20,
+  },
+  {
+    title: 'สูตร 21',
+    description: 'นมเปรี้ยว + กีวี่',
+    imageUrl: img21,
+  },
+  {
+    title: 'สูตร 22',
+    description: 'นมเปรี้ยว + แตงโม',
+    imageUrl: img22,
+  },
+  {
+    title: 'สูตร 23',
+    description: 'นมเปรี้ยว + ส้ม',
+    imageUrl: img23,
+  },
+  {
+    title: 'สูตร 24',
+    description: 'นมเปรี้ยว + บ๊วย',
+    imageUrl: img24,
+  },
+  {
+    title: 'สูตร 25',
+    description: 'นมเปรี้ยว + น้ำมะนาว',
+    imageUrl: img25,
+  },
+  {
+    title: 'สูตร 26',
+    description: 'นมเปรี้ยว + คาราเมล',
+    imageUrl: img26,
+  },
+  {
+    title: 'สูตร 27',
+    description: 'นมเปรี้ยว + ลิ้นจี่',
+    imageUrl: img27,
+  },
+  {
+    title: 'สูตร 28',
+    description: 'นมเปรี้ยว + มัทฉะ',
+    imageUrl: img28,
+  },
+  {
+    title: 'สูตร 29',
+    description: 'นมเปรี้ยว + มะละกอ',
+    imageUrl: img29,
+  },
+  {
+    title: 'สูตร 30',
+    description: 'นมเปรี้ยว + นมหมี',
+    imageUrl: img30,
+  },
+  {
+    title: 'สูตร 31',
+    description: 'นมเปรี้ยว + เสาวรส',
+    imageUrl: img31,
+  },
+  {
+    title: 'สูตร 32',
+    description: 'นมเปรี้ยว + ไข่มุก',
+    imageUrl: img32,
+  },
+  {
+    title: 'สูตร 33',
+    description: 'นมเปรี้ยว + อัญชัน',
+    imageUrl: img33,
+  },
+  {
+    title: 'สูตร 34',
+    description: 'นมเปรี้ยว + โยเกิร์ต',
+    imageUrl: img34,
+  },
+  {
+    title: 'สูตร 35',
+    description: 'นมเปรี้ยว + โซดา',
+    imageUrl: img35,
+  },
+  {
+    title: 'สูตร 36',
+    description: 'นมเปรี้ยวกับคอมบูชะ',
+    imageUrl: img36,
+  },
+  {
+    title: 'สูตร 37',
+    description: 'นมเปรี้ยวกับข้าวโอ๊ต',
+    imageUrl: img37,
+  },
+  {
+    title: 'สูตร 38',
+    description: 'นมเปรี้ยว + ฟักทอง',
+    imageUrl: img38,
+  },
+  {
+    title: 'สูตร 39',
+    description: 'นมเปรี้ยวกับพีชครับ',
+    imageUrl: img39,
+  },
+  {
+    title: 'สูตร 40',
+    description: 'นมเปรี้ยว + แครอท',
+    imageUrl: img40,
+  },
+ 
+  {
+    title: 'สูตร 41',
+    description: 'อัญชันนมเปรี้ยวโยเกิร์ต',
+    imageUrl: img41,
+  },
+ 
+  {
+    title: 'สูตร 42',
+    description: 'นมเปรี้ยว + อกไก่ต้ม',
+    imageUrl: img42,
+  },
+ 
+  {
+    title: 'สูตร 43',
+    description: 'นมเปรี้ยว + น้ำเต้าหู้',
+    imageUrl: img43,
+  },
+ 
+  {
+    title: 'สูตร 44',
+    description: 'นมเปรี้ยว + แคนตาลูป',
+    imageUrl: img44,
+  },
+ 
+  {
+    title: 'สูตร 45',
+    description: 'นมเปรี้ยว + ผักกาดหอม',
+    imageUrl: img45,
+  },
+ 
+  {
+    title: 'สูตร 46',
+    description: 'นมเปรี้ยว + แตงกวา',
+    imageUrl: img46,
+  },
+ 
+  {
+    title: 'สูตร 47',
+    description: 'นมเปรี้ยว + มะเขือเทศ',
+    imageUrl: img47,
+  },
+  {
+    title: 'สูตร 48',
+    description: 'นมเปรี้ยว + ไอศรีมวนิลา',
+    imageUrl: img48,
+  },
+  {
+    title: 'สูตร 49',
+    description: 'นมเปรี้ยว + ลอดช่อง',
+    imageUrl: img49,
+  },
+  {
+    title: 'สูตร 50',
+    description: 'นมเปรี้ยว + อัลมอนด์',
+    imageUrl: img50,
+  },
+  {
+    title: 'สูตร 51',
+    description: 'นมเปรี้ยว + ครีมชีส',
+    imageUrl: img51,
+  },
+  {
+    title: 'สูตร 52',
+    description: 'นมเปรี้ยว + ขึ้นฉ่าย',
+    imageUrl: img52,
+  },
+  {
+    title: 'สูตร 53',
+    description: 'มะพร้าวนมเปรี้ยวอัญชัน',
+    imageUrl: img53,
+  },
+  {
+    title: 'สูตร 54',
+    description: 'นมเปรี้ยว + ลาเต้',
+    imageUrl: img54,
+  },
+  {
+    title: 'สูตร 55',
+    description: 'นมเปรี้ยว + มะขาม',
+    imageUrl: img55,
+  },
+  {
+    title: 'สูตร 56',
+    description: 'นมเปรี้ยวเรนโบว์',
+    imageUrl: img56,
+  },
+  {
+    title: 'สูตร 57',
+    description: 'นมเปรี้ยว + ทุเรียน',
+    imageUrl: img57,
+  },
+  {
+    title: 'สูตร 58',
+    description: 'นมเปรี้ยว + ข้าวโพด',
+    imageUrl: img58,
+  },
+  {
+    title: 'สูตร 59',
+    description: 'นมเปรี้ยว + มะพร้าว',
+    imageUrl: img59,
+  },
+ 
 ];
 
 export default messages;
